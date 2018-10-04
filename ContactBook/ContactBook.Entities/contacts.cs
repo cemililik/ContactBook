c553c5000fb3468a8d1b4f8658efe51e7ee5481b
Cemil ILIK <cemililik@outlook.com>
2018-10-04 10:50:33 +0300

    basic structure

---
+++ ContactBook/ContactBook.Entities/contacts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBook.Entities
+{
+    public class contacts
+    {
+        public Guid id { get; set; }
+        public string cName { get; set; }
+        public string cSurname { get; set; }
+        public string numberI { get; set; }
+        public string numberII { get; set; }
+        public string numberIII { get; set; }
+        public string emailAdress { get; set; }
+        public string webAdress { get; set; }
+        public string adress { get; set; }
+        public string info { get; set; }
+    }
+}
---
+++ ContactBook/ContactBook.Entities/users.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBook.Entities
+{
+    public class users
+    {
+        public Guid userId { get; set; }
+        public string userName { get; set; }
+        public string pass { get; set; }
+    }
+}
