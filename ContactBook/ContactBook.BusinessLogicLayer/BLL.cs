6c9b71cd3b47d96881d96690d5dcede592d42c2b
Cemil ILIK <cemililik@outlook.com>
2018-10-04 12:51:52 +0300

    BusinessLogicLayer & DatabaseLogicLayer

---
+++ ContactBook/ContactBook.BusinessLogicLayer/BLL.cs
@@ -0,0 +1,159 @@
+﻿using ContactBook.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBook.BusinessLogicLayer
+{
+    public class BLL
+    {
+        DatabaseLogicLayer.DLL dll;
+
+        public BLL()
+        {
+            dll = new DatabaseLogicLayer.DLL();
+        }
+
+        public int Authentication(string userName, string pass)
+        {
+            if (!string.IsNullOrEmpty(userName) && !string.IsNullOrEmpty(pass))
+            {
+                return dll.Authentication(new Entities.users()
+                {
+                    userName = userName,
+                    pass = pass
+                });
+
+            }
+            else
+                return -1;
+        }
+
+        public int addRecord(string cName, string cSurname, string numberI, string numberII, string numberIII, string emailAdress, string webAdress, string adress, string info)
+        {
+            if (!string.IsNullOrEmpty(cName) && !string.IsNullOrEmpty(cSurname) && !string.IsNullOrEmpty(numberI))
+            {
+                return dll.addContact(new Entities.contacts()
+                {
+                    id = Guid.NewGuid(),
+                    cName = cName,
+                    cSurname=cSurname,
+                    numberI=numberI,
+                    numberII=numberII,
+                    numberIII=numberIII,
+                    emailAdress=emailAdress,
+                    webAdress=webAdress,
+                    adress=adress,
+                    info=info
+                });
+            }
+            else
+                return -1;
+        }
+
+        public int updateRecord(Guid id, string cName, string cSurname, string numberI, string numberII, string numberIII, string emailAdress, string webAdress, string adress, string info)
+        {
+            if (id != Guid.Empty)
+            {
+                return dll.updateContact(new Entities.contacts()
+                {
+                    id = id,
+                    cName = cName,
+                    cSurname = cSurname,
+                    numberI = numberI,
+                    numberII = numberII,
+                    numberIII = numberIII,
+                    emailAdress = emailAdress,
+                    webAdress = webAdress,
+                    adress = adress,
+                    info = info
+                });
+            }
+            else return -1;
+        }
+
+        public int deleteContact(Guid id)
+        {
+            if (id != Guid.Empty)
+            {
+                return dll.deleteContact(id);
+            }
+            else return -1;
+        }
+
+        public List<contacts> listContact()
+        {
+            List<contacts> contactList = new List<contacts>(); 
+            try
+            {
+                SqlDataReader reader = dll.listContact();
+                while (reader.Read())
+                {
+                    contactList.Add(new contacts()
+                    {
+                        id = reader.IsDBNull(0) ? Guid.Empty : reader.GetGuid(0),
+                        cName = reader.IsDBNull(1) ? string.Empty : reader.GetString(1),
+                        cSurname = reader.IsDBNull(2) ? string.Empty : reader.GetString(2),
+                        numberI = reader.IsDBNull(3) ? string.Empty : reader.GetString(3),
+                        numberII = reader.IsDBNull(4) ? string.Empty : reader.GetString(4),
+                        numberIII = reader.IsDBNull(5) ? string.Empty : reader.GetString(5),
+                        emailAdress = reader.IsDBNull(6) ? string.Empty : reader.GetString(6),
+                        webAdress = reader.IsDBNull(7) ? string.Empty : reader.GetString(7),
+                        adress = reader.IsDBNull(8) ? string.Empty : reader.GetString(8),
+                        info = reader.IsDBNull(9) ? string.Empty : reader.GetString(9)
+                    });
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                dll.SetConnection();
+            }
+            return contactList;
+        }
+
+        public contacts listContactId(Guid id)
+        {
+            contacts contactListId = new contacts();
+            try
+            {
+                SqlDataReader reader = dll.listContactId(id);
+                while (reader.Read())
+                {
+                    contactListId = new contacts()
+                    {
+                        id = reader.IsDBNull(0) ? Guid.Empty : reader.GetGuid(0),
+                        cName = reader.IsDBNull(1) ? string.Empty : reader.GetString(1),
+                        cSurname = reader.IsDBNull(2) ? string.Empty : reader.GetString(2),
+                        numberI = reader.IsDBNull(3) ? string.Empty : reader.GetString(3),
+                        numberII = reader.IsDBNull(4) ? string.Empty : reader.GetString(4),
+                        numberIII = reader.IsDBNull(5) ? string.Empty : reader.GetString(5),
+                        emailAdress = reader.IsDBNull(6) ? string.Empty : reader.GetString(6),
+                        webAdress = reader.IsDBNull(7) ? string.Empty : reader.GetString(7),
+                        adress = reader.IsDBNull(8) ? string.Empty : reader.GetString(8),
+                        info = reader.IsDBNull(9) ? string.Empty : reader.GetString(9)
+                    };
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                dll.SetConnection();
+            }
+            return contactListId;
+        }
+
+    }
+}
---
+++ ContactBook/ContactBook.DatabaseLogicLayer/DLL.cs
@@ -0,0 +1,150 @@
+﻿using ContactBook.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactBook.DatabaseLogicLayer
+{
+    public class DLL
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader reader;
+
+        int ReturnValues;
+
+        public DLL()
+        {
+            con = new SqlConnection("server=localhost; Authentication=Windows Authentication; Database=ContactBook ");
+        }
+        public void SetConnection()
+        {
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            else
+                con.Close();
+        }
+
+        public int Authentication(users u)
+        {
+            try
+            {
+                cmd = new SqlCommand("select count(*) from users where userName=@userName and pass=@pass", con);
+                cmd.Parameters.Add("@userName", SqlDbType.NVarChar).Value = u.userName;
+                cmd.Parameters.Add("@pass", SqlDbType.NVarChar).Value = u.pass;
+                SetConnection();
+                ReturnValues = (int)cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+
+                
+            }
+            finally
+            {
+                SetConnection();
+            }
+            return ReturnValues;
+        }
+
+        public int addContact(contacts c)
+        {
+            try
+            {
+                cmd = new SqlCommand("insert into contacts (ID,cName,cSurname,numberI,numberII,numberIII,emailAdress,webAdress,adress,info) values (@ID,@cName,@cSurname,@numberI,@numberII,@numberIII,@emailAdress,@webAdress,@adress,@info)", con);
+                cmd.Parameters.Add("@ID", SqlDbType.UniqueIdentifier).Value = c.id;
+                cmd.Parameters.Add("@cName", SqlDbType.NVarChar).Value = c.cName;
+                cmd.Parameters.Add("@cSurname", SqlDbType.NVarChar).Value = c.cSurname;
+                cmd.Parameters.Add("@numberI", SqlDbType.NVarChar).Value = c.numberI;
+                cmd.Parameters.Add("@numberII", SqlDbType.NVarChar).Value = c.numberII;
+                cmd.Parameters.Add("@numberIII", SqlDbType.NVarChar).Value = c.numberIII;
+                cmd.Parameters.Add("@emailAdress", SqlDbType.NVarChar).Value = c.emailAdress;
+                cmd.Parameters.Add("@webAdress", SqlDbType.NVarChar).Value = c.webAdress;
+                cmd.Parameters.Add("@adress", SqlDbType.NVarChar).Value = c.adress;
+                cmd.Parameters.Add("@info", SqlDbType.NVarChar).Value = c.info;
+                SetConnection();
+                ReturnValues = cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                SetConnection();
+            }
+            return ReturnValues; 
+        }
+
+        public int updateContact(contacts c)
+        {
+            try
+            {
+                cmd = new SqlCommand(@"update contacts cName = @cName,cSurname = @cSurname, numberI = @numberI, numberII = @numberII, numberIII = @numberIII, emailAdress = @emailAdress, webAdress = @webAdress, adress = @adress, info = @info where id= @id", con);
+                cmd.Parameters.Add("@ID", SqlDbType.UniqueIdentifier).Value = c.id;
+                cmd.Parameters.Add("@cName", SqlDbType.NVarChar).Value = c.cName;
+                cmd.Parameters.Add("@cSurname", SqlDbType.NVarChar).Value = c.cSurname;
+                cmd.Parameters.Add("@numberI", SqlDbType.NVarChar).Value = c.numberI;
+                cmd.Parameters.Add("@numberII", SqlDbType.NVarChar).Value = c.numberII;
+                cmd.Parameters.Add("@numberIII", SqlDbType.NVarChar).Value = c.numberIII;
+                cmd.Parameters.Add("@emailAdress", SqlDbType.NVarChar).Value = c.emailAdress;
+                cmd.Parameters.Add("@webAdress", SqlDbType.NVarChar).Value = c.webAdress;
+                cmd.Parameters.Add("@adress", SqlDbType.NVarChar).Value = c.adress;
+                cmd.Parameters.Add("@info", SqlDbType.NVarChar).Value = c.info;
+                SetConnection();
+                ReturnValues = cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                SetConnection();
+            }
+            return ReturnValues;
+        }
+
+        public int deleteContact(Guid id)
+        {
+            try
+            {
+                cmd = new SqlCommand(@"delete contacts where id = @id", con);
+                cmd.Parameters.Add("@ID", SqlDbType.UniqueIdentifier).Value = id;
+                SetConnection();
+                ReturnValues = cmd.ExecuteNonQuery();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                SetConnection();
+            }
+            return ReturnValues;
+        }
+
+        public SqlDataReader listContact()
+        {
+            cmd = new SqlCommand("select * from contacts", con);
+            SetConnection();
+            return cmd.ExecuteReader();
+        }
+
+        public SqlDataReader listContactId(Guid id)
+        {
+            cmd = new SqlCommand("select * from contacts where id=@id", con);
+            cmd.Parameters.Add("@id", SqlDbType.UniqueIdentifier).Value = id;
+            SetConnection();
+            return cmd.ExecuteReader();
+        }
+    }
+}
